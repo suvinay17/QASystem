e31d75165625552790dda27ea7d626b8612d5356
suvinay17 <suvinay.bothra@richmond.edu>
2020-10-23 00:57:37 -0400

    Normalized wf

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ def parseTopDocs(data):
 
 """
     cosineSimilarity(X, Y) returns the cosine similarity of two vectors X and Y
-    Input: Vector X which is the question feature vector and Vector Y which is the answer feature vecotr
+    Input: Vector X -> which is the question feature vector and Vector Y which is the answer feature vecotr
     Output: The cosine similarity of vectors X and Y returned as 2d array
 """
 def cosineSimilarity(X, Y):
@@ -82,8 +82,8 @@ def cosineSimilarity(X, Y):
 
 """
     corpusCounts(corpus) returns the counts of each token in every chunk of the corpus.
-    Input: corpus: List of chunks (sentences) from the corpus
-    Output: X: 2d array of counts for each token in a chunk
+    Input: corpus -> List of chunks (sentences) from the corpus
+    Output: X -> 2d array of counts for each token in a chunk
 """
 def corpusCounts(corpus):
     vectorizer = CountVectorizer()
@@ -94,23 +94,24 @@ def corpusCounts(corpus):
 
 """
     corpusCounts(corpus) returns the normalizedWordFrequency of each token for each chunk : count/ size of sentence as X, and binary counts for each token in a chunk as Y
-    Input: corpus: List of chunks (sentences) from the corpus, X
-    Output: X: 2d array with normalizedWordFrequency for each token in a chunk (feature matrix)
-            Y: 2d array of binary counts for each token in a chunk. (feature matrix)
+    Input: corpus -> List of chunks (sentences) from the corpus, X
+    Output: X -> 2d array with normalizedWordFrequency for each token in a chunk (feature matrix)
+            Y -> 2d array of binary counts for each token in a chunk. (feature matrix)
 """
 def normalizedWordFrequencyMatrix(corpus, X):
-    Y = [[]] #Might Change X, Y to Numpy Arrays
+    Y = [[0]*len(X[0])]*len(X) #Might Change X, Y to Numpy Arrays
     for i in range(len(X)):
+        c = len(word_tokenize(corpus[i]))
         for j in range(len(X[0])):
-            X[i][j] = X[i][j]/len(corpus[i])
-            Y[i][j] = 1 if x[i][j] > 0 else 0
+            X[i][j] = X[i][j]/c
+            Y[i][j] = 1 if X[i][j] > 0 else 0
     return X,Y
 
 
 """
     removeStopWords(sentence) removes the words that do not carry much semantic meaning and returns a list of words that are not stop words
-    Input: sentence: string to remove stop words from
-    Output: filtered_sentence: list with only relevant tokens from sentence
+    Input: sentence -> string to remove stop words from
+    Output: filtered_sentence -> list with only relevant tokens from sentence
 """
 def removeStopWords(sentence):
     stop_words = set(stopwords.words('english'))
@@ -124,6 +125,14 @@ question_dict = parseQuestions(data[0])
 # print(data[0])
 id_dict = parseRelevantDocs(data[1])
 # print(id_dict)
+corpus = [
+'This is the first document.',
+'This document is the second document.',
+'And this is the third one.',
+'Is this the first document?']
 topdoc_data = getData("training/topdocs/", 1)
+X = corpusCounts(corpus)
+A,B = normalizedWordFrequencyMatrix(corpus, X)
+print(B)
 # print(topdoc_data[0])
 # parseTopDocs(topdoc_data)
